734aa5601889b5923153b9d47f644c0cb6d73a84
Eezi <tomi.a.salo@outlook.com>
2020-09-19 18:47:10 +0300

    Makeing cards component

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-function shadowmaker(options){
+function shadowMaker(options){
     let images = document.querySelectorAll('.shadowmaker');
 
-    if(options.shadow_type === 'hard')
-        options.shadow_type = '0px'
-    else
-        options.shadow_type = '15px'
-    
+    if(options.shadow_type === 'small')
+        options.shadow_type = '5px'
+    else if(options.shadow_type === 'medium')
+        options.shadow_type = '20px'
+    else if(options.shadow_type === 'large')
+        options.shadow_type = '50px'
     images.forEach(image => {
         image.style.boxShadow = `10px 10px ${options.shadow_type} 1px rgba(0,0,0,0.12)`;
 
@@ -15,4 +16,24 @@ function shadowmaker(options){
     })
 }
 
-module.exports.shadowmaker = shadowmaker;
+function cardMaker(options){
+    let cards = document.querySelectorAll('.cardMaker');
+
+    if(options.card_type === 'small')
+        options.card_type = '50px'
+
+    else if(options.card_type === 'medium') 
+        options.cardMaker = '100px'
+    
+    cards.forEach(card => {
+        card.style.height = options.card_type
+        card.style.width = options.card_type
+        card.style.backgroundColor = '#394359'
+        card.style.border = '1px solid #F2BE8D'
+    });
+}
+
+module.exports = {
+    shadowMaker,
+    cardMaker
+}
---
+++ test.js
@@ -0,0 +1,10 @@
+import { shadowMaker, cardMaker } from './index.js'
+
+shadowMaker({
+    padding: true,
+    shadow_type: 'medium'
+});
+
+cardMaker({
+    card_type: 'small'
+})
